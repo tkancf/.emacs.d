c5d8d7a3968419b6ea0f4092dbf651a47bf5f333
tkancf <18424095+tkancf@users.noreply.github.com>
2024-03-23 14:47:00 +0900

    Add some packages

--- init.el
+++ init.el
@@ -49,6 +49,93 @@
   ;; テーマの有効化
   (load-theme 'monokai t))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; which-keyのインストール
+(leaf which-key
+  :ensure t
+  :config
+  (which-key-mode 1))
+
+;; ダッシュボードの変更
+(leaf dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook)
+  (setq dashboard-items '((recents  . 5)
+                          (bookmarks . 5)
+                          (projects . 5))))
+
+;; 自動補完の設定
+(leaf corfu
+  :ensure t
+  :custom
+  ((corfu-cycle . t) ;; 候補のループを有効にする
+   (corfu-auto . t) ;; 自動補完を有効にする
+   (corfu-auto-prefix . 2) ;; 補完候補を二文字で出す
+   )
+  :global-minor-mode global-corfu-mode)
+
+;; vertico - M-xをファジーファインダーっぽくする
+(leaf vertico
+  :ensure t
+  :custom
+  (vertico-cycle . t) ;; 一番下の選択肢からさらに下に行こうとすると最初に戻る
+  :init
+  (vertico-mode 1))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; org-modeの設定
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; orgのディレクトリ設定
+;; org-directoryの設定
+(setq org-dir "~/Dropbox/org/")
+
+(leaf org
+  :custom
+  (org-directory . org-dir)
+  :config
+  ;; org-captureのテンプレート
+  (setq org-capture-templates
+        `(("t" "Todo" entry (file+headline ,(concat org-dir "todo.org") "Todo")
+           "* TODO %?\n  %i\n  %a")
+          ("n" "Note" entry (file+headline ,(concat org-dir "memo.org") "Memo")
+           "* %?\nEntered on %U\n  %i\n  %a")
+          ("j" "Journal" entry (file+headline ,(concat org-dir "journal.org") "Journal")
+           "* %U\n%?\n%i\n")))
+
+  ;; org-agendaのファイル
+  (setq org-agenda-files (list (concat org-directory "tasks.org")
+                               (concat org-directory "calendar.org")))
+
+  ;; org-modeのキー設定
+  (global-set-key "\C-cc" 'org-capture) ;; org-capture
+  (define-key global-map "\C-ca" 'org-agenda)) ;; org-agenda
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; オリジナルキーマップ
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; カーソル位置から行頭まで削除する
+(leaf *backward-kill-line
+  :config
+  (defun backward-kill-line (arg)
+  "Kill chars backward until encountering the end of a line."
+  (interactive "p")
+  (kill-line 0))
+  ;; C-uに設定
+  (global-set-key (kbd "C-u") 'backward-kill-line)
+  ;; 元のC-uの挙動をC-.へ割当
+  (global-set-key (kbd "C-.") 'universal-argument))
+
+(leaf *disable-ctrl-x-ctrl-c
+  :config
+  (global-set-key (kbd "C-x C-c") 'ignore))
+
+
 (leaf leaf
   :config
   (leaf leaf-convert :ensure t)
